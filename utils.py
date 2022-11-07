981853f6860462bd427d4536aacce9f3a8bf6a88
Rafael <rmateusc@eafit.edu.co>
2022-11-06 19:14:38 -0500

    update utils structure

--- utils.py
+++ utils.py
@@ -4,21 +4,21 @@ import pandas as pd
 import geopandas as gpd
 import shapely.geometry
 import matplotlib.pyplot as plt
-from shapely.geometry import MultiPolygon, Polygon, Point
+from shapely.geometry import Point
 
 # Creates a square grid according to a map
 def unbounded_grid(shapefile, n_cells=30):
     # define bounds
     xmin, ymin, xmax, ymax = shapefile.total_bounds
-    cell_size = (xmax-xmin)/n_cells
+    cell_size = (xmax-xmin) / n_cells
 
     grid_cells = []
     for x0 in np.arange(xmin, xmax+cell_size, cell_size):
         for y0 in np.arange(ymin, ymax+cell_size, cell_size):
             # bounds
-            x1 = x0-cell_size
-            y1 = y0+cell_size
-            grid_cells.append( shapely.geometry.box(x0, y0, x1, y1)  )
+            x1 = x0 - cell_size
+            y1 = y0 + cell_size
+            grid_cells.append(shapely.geometry.box(x0, y0, x1, y1))
     cell = gpd.GeoDataFrame(grid_cells, columns=['geometry'])
     return cell
 
@@ -34,15 +34,14 @@ def bounded_grid(shapefile, cells):
 
     # Add new column with boolean
     grid['in_map'] = in_shape
-
     # Only use grid cells that are within the map
     bounded = grid[grid.in_map==True].copy()
+
     return bounded
 
 def geometry_union(gdf):
     # Create a multipolygon from various polygons
     multipol = gdf.geometry.unary_union
-
     # Unify into one multipolygon
     full_polygon = gpd.GeoDataFrame(geometry=[multipol], crs=gdf.crs)
 
@@ -76,16 +75,12 @@ def plot_map_points(mapa, points, figsize=(7, 7)):
 
 # Create random points inside bounds
 def create_randpoints(shapefile, N):
-
     # Define bounds
-    xmin=shapefile.geometry.bounds.minx
-    ymin=shapefile.geometry.bounds.miny
-    xmax=shapefile.geometry.bounds.maxx
-    ymax=shapefile.geometry.bounds.maxy
+    xmin, xmax = shapefile.geometry.bounds.minx, shapefile.geometry.bounds.maxx
+    ymin, ymax = shapefile.geometry.bounds.miny, shapefile.geometry.bounds.maxy
 
     #Empty DataFrame
     random_points = pd.DataFrame()
-
     #Empty lists
     random_x=[]
     random_y=[]
@@ -102,6 +97,7 @@ def create_randpoints(shapefile, N):
 
     random_points['x']=random_x
     random_points['y']=random_y
+
     return random_points
 
 # Join coordinates with Point geometry
